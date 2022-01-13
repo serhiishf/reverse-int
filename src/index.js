0369ebec3d220eab1f44694b1910ba964623ef79
Сергей Ш <unison447@gmail.com>
2022-01-13 20:54:19 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = n * -1;
+    }
+    n = String(n);
+    let result = '';
+    for (let i = 0; i < n.length; i++) {
+        result = n[i] + result;
+    }
+    result = Number(result);
+    return result;
 }
